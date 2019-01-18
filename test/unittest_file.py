e1817d955409e3e10c78bdc8db9926f0ed88b4b5
Brendan McCray <rbmccr@gmail.com>
2019-01-18 15:39:45 -0600

    finished unit testing 1 thru 4

--- test/unittest_file.py
+++ test/unittest_file.py
@@ -16,9 +16,9 @@ class Testing(unittest.TestCase):
 
 # ==================================================================
 
-# test if items can be added to bag, and assigned to a child.
+# 1. test if items can be added to bag, and assigned to a child.
 
-  def test_add_toy(self):
+  def test01_add_toy(self):
     # function returns same ID that is passed in
     self.assertEqual(self.lootbag.add("Hotwheels", 1), 1)
     # database table will have toyID of 1
@@ -28,10 +28,10 @@ class Testing(unittest.TestCase):
 
 # ==================================================================
 
-# Items can be removed from bag, per child.
+# 2. Items can be removed from bag, per child.
 # Removing a toy from the bag should not be allowed. A child's name must be specified.
 
-  def test_remove_toy(self):
+  def test02_remove_toy(self):
     # database has a toy with the name Hotwheels, and its ID is 1
     self.assertEqual(self.lootbag.get_toyID("Hotwheels"), 1)
     # database has a child "Brendan" with an ID of 1
@@ -41,6 +41,24 @@ class Testing(unittest.TestCase):
     # removing the toy results in database response of toyID = None
     self.lootbag.remove(1)
     self.assertEqual(self.lootbag.get_toyID("Hotwheels"), None)
+
+# 3. Must be able to list all children who are getting a toy.
+# 4. Must be able to list all toys for a given child's name.
+
+  def test03_child_list(self):
+    # Three children receive are added in this example
+    self.lootbag.add("Rocket", 1)
+    self.lootbag.add("Robot", 2)
+    self.lootbag.add("Plane", 5)
+    # list function will fail to execute if child arg is not specified
+    self.assertEqual(self.lootbag.ls(None, False), "No child ID specified")
+    # a list of three names is returned if listing all children (id = false for a list of multiple children) - note delivered status of TRUE is required
+    self.lootbag.update_child_delivery_status(True, 1)
+    self.lootbag.update_child_delivery_status(True, 2)
+    self.lootbag.update_child_delivery_status(True, 5)
+    self.assertEqual(self.lootbag.ls(False, False), ["Brendan","Zac","Brad"])
+    # a child's toys are displayed when child ID is explicitly provided
+    self.assertEqual(self.lootbag.ls(1, False), "Rocket")
 
 if __name__ == '__main__':
   unittest.main()
