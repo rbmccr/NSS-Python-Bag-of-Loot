8977f57c2fe94a2ecd2e4798dc6df5b68f8aee49
Brendan McCray <rbmccr@gmail.com>
2019-01-17 21:46:59 -0600

    finished #1 and fixed database locked bug by returning child id from add function

--- lootbag.py
+++ lootbag.py
@@ -7,7 +7,7 @@ database = 'bag_o_loot.db'
 
 # 0. Class created for establishing child props and method to add to database
 class Child:
-  def __init__(self, name, delivered = 0):
+  def __init__(self, name, delivered=0):
     self.name = name
     self.delivered = delivered
 
@@ -36,13 +36,12 @@ class Child:
 # 1. Add a toy to the bag o' loot, and label it with the child's name who will receive it. The first argument must be the word add. The second argument is the gift to be delivered. The third argument is the name of the child.
 
 def add():
+
   toy = {
     "name": sys.argv[2],
     # get child's ID from db using name in command line
     "childID": get_childID(sys.argv[3])
   }
-
-  print(toy["childID"])
 
   with sqlite3.connect(database) as conn:
     cursor = conn.cursor()
@@ -55,7 +54,7 @@ def add():
       )
 
       # set child's delivered status to 1
-      update_child_delivery_status(True, toy["childID"])
+      return toy["childID"]
 
     except sqlite3.OperationalError as err:
       print("Error when attempting to post toy to database...", err)
@@ -90,7 +89,9 @@ def get_childID(name):
 # Update delivered status if a child has received his/her one gift
 # revoke delivered status if child has no gift in the database
 def update_child_delivery_status(adding_item, child_id):
-  print("updating delivered status")
+
+  print("updating delivered status...")
+
   if adding_item == True:
     with sqlite3.connect(database) as conn:
       cursor = conn.cursor()
@@ -108,7 +109,8 @@ def update_child_delivery_status(adding_item, child_id):
 # trigger function call based on command line input
 if sys.argv[1] == 'add':
   print("adding toy to database...")
-  add()
+  id_from_add = add()
+  update_child_delivery_status(True, id_from_add)
 
 if sys.argv[1] == 'remove':
   print("removing toy from database...")
